c388819358bf80bd9c965bde22371444c69682dd
Ashlee Crusco <ashlee@flatironschool.com>
2018-02-15 15:42:15 -0500

    auth flow complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+import { Button } from 'semantic-ui-react'
+
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button as="a" href="http://localhost:3001/api/v1/login">Log In</Button>
       </div>
-    );
+    )
   }
 }
 
